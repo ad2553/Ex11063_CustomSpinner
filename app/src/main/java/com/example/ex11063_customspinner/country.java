f38d72a7b62d9569a2ae33ff6b92a61e90f09ddb
ad2553 <ad2553@bs.amalnet.k12.il>
2024-11-25 15:50:37 +0200

    xml and country complete

--- app/src/main/java/com/example/ex11063_customspinner/country.java
+++ app/src/main/java/com/example/ex11063_customspinner/country.java
@@ -2,5 +2,36 @@ package com.example.ex11063_customspinner;
 
 public class country
 {
-    
+    int flag;
+    String countryName;
+    String capital;
+    int population;
+
+    public country(int flag, String countryName, String capital, int population)
+    {
+        this.flag = flag;
+        this.countryName = countryName;
+        this.capital = capital;
+        this.population = population;
+    }
+
+    public String getCountryName()
+    {
+        return countryName;
+    }
+
+    public int getFlag()
+    {
+        return flag;
+    }
+
+    public int getPopulation()
+    {
+        return population;
+    }
+
+    public String getCapital()
+    {
+        return capital;
+    }
 }
